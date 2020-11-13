12c61113072e167a1f2df5ede570f29df374e5e4
insaafdhansay <insaafdhansay@gmail.com>
2020-11-13 18:11:12 +0200

    Form updated- checklist added

--- app/src/pages/Capture.js
+++ app/src/pages/Capture.js
@@ -9,8 +9,49 @@ import Button from "react-bootstrap/Button";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 import "./DailyCapture.css";
+import Checklist from "../components/Checklist";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { RadioGroup } from "@material-ui/core";
+import Radio from "@material-ui/core/Radio";
 
-
+const symptomsData = [
+  {
+    text: "Cough",
+  },
+  {
+    text: "Sore Throat ",
+  },
+  {
+    text: "Shortness of breath ",
+  },
+  {
+    text: "Body aches ",
+  },
+  {
+    text: "Loss of taste ",
+  },
+  {
+    text: "Nausea  ",
+  },
+  {
+    text: "Vomiting ",
+  },
+  {
+    text: "Diarrhoea ",
+  },
+  {
+    text: "Fatigue ",
+  },
+  {
+    text: "Weakness or tiredness ",
+  },
+];
+const certData = [
+  {
+    text: "I Agree",
+  },
+];
 export default class Capture extends Component {
   constructor(props) {
     super(props);
@@ -18,13 +59,26 @@ export default class Capture extends Component {
     //State is used instead of class member variables to avoid manually managing component renders
     this.state = {
       empID: "",
-      empTemp:"",
-      empTravel:"",
-      empExposure:"",
-      empExposure:"",
-
+      empTemp: "",
+      empTravel: "0",
+      empExposure: "0",
+      empCert: "",
+      selectedSymptoms: [],
+      selectedCert: [],
     };
+    this.callbackChecklist = this.callbackChecklist.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+  //Sets the state of the symptoms array to include items that have been selected.
+  callbackChecklist = (checked) => {
+    this.setState({ selectedSymptoms: checked });
+  };
+  callbackChecklistCert = (checked) => {
+    this.setState({ selectedCert: checked });
+  };
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render() {
     return (
       <div style={{ overflowX: "none" }}>
@@ -47,107 +101,127 @@ export default class Capture extends Component {
                         required={true}
                         name="empID"
                         value={this.state.empID}
+                        onChange={this.handleChange}
                       />
                     </Form.Group>
                   </div>
                 </div>
-                <hr /> 
-                  {/*Div used for Symptoms  */}
-                  <div className="u-div">
+                <hr />
+                {/*Div used for Symptoms  */}
+                <div className="u-div">
                   <div className="ulabel">Symptoms</div>
                   <div className="ucontent-div">
-                    <Form.Group as={Col} controlId="checklist edit">
-                      <OverlayTrigger placement="left">
-                        <Form.Label>Identification Number</Form.Label>
-                      </OverlayTrigger>
-                      <Form.Control
-                        placeholder="will be checklist"
-                        required={true}
-                        name="checklist"
-                        value={this.state.empID}
+                    <Form.Group as={Col} controlId="symptonsChecklist">
+                      <Form.Label>Symptoms</Form.Label>
+
+                      <Checklist
+                        data={symptomsData.map((symptoms) => symptoms.text)}
+                        checked={this.state.selectedSymptoms}
+                        callback={this.callbackChecklist}
                       />
                     </Form.Group>
                   </div>
                 </div>
-                <hr /> 
-                  {/*Div used for General */}
-                  <div className="u-div">
+                <hr />
+                {/*Div used for General */}
+                <div className="u-div">
                   <div className="ulabel">General</div>
                   <div className="ucontent-div">
                     <Form.Group as={Col} controlId="employeeTemp">
-                      <OverlayTrigger placement="left">
-                        <Form.Label>Your Temperature</Form.Label>
-                      </OverlayTrigger>
+                      <Form.Label>Your Temperature</Form.Label>
+
                       <Form.Control
                         placeholder="Enter your temperature today"
                         required={true}
                         name="empTemp"
                         value={this.state.empTemp}
+                        onChange={this.handleChange}
                       />
                     </Form.Group>
-                 
-          
+
                     <Form.Group as={Col} controlId="exposure">
-                      <OverlayTrigger placement="left">
-                        <Form.Label>In the past 14 days, have you been in close proximity to anyone who has tested positive for COVID-19?</Form.Label>
-                      </OverlayTrigger>
-                      <Form.Control
-                        placeholder="radio buttons Y and N"
-                        required={true}
-                        name="empExposure"
-                        value={this.state.empExposure}
-                      />
+                      <Form.Label>
+                        In the past 14 days, have you been in close proximity to
+                        anyone who has tested positive for COVID-19?
+                      </Form.Label>
+
+                      <FormControl component="fieldset">
+                        <RadioGroup
+                          value={this.state.empExposure}
+                          onChange={this.handleChange}
+                        >
+                          <FormControlLabel
+                            value="0"
+                            control={<Radio style={{ color: "#32CD32" }} />}
+                            label="No"
+                          />
+                          <FormControlLabel
+                            value="1"
+                            control={<Radio style={{ color: "#32CD32" }} />}
+                            label="Yes"
+                          />
+                        </RadioGroup>
+                      </FormControl>
                     </Form.Group>
-               
-          
+
                     <Form.Group as={Col} controlId="travel">
-                      <OverlayTrigger placement="left">
-                        <Form.Label>Have you recently travelled to any high-risk country or any high-risk area defined under the National Disaster Regulations? </Form.Label>
-                      </OverlayTrigger>
-                      <Form.Control
-                        placeholder="radio buttons Y and N"
-                        required={true}
-                        name="empTravel"
-                        value={this.state.empTravel}
-                      />
+                      <Form.Label>
+                        Have you recently travelled to any high-risk country or
+                        any high-risk area defined under the National Disaster
+                        Regulations?{" "}
+                      </Form.Label>
+
+                      <FormControl component="fieldset">
+                        <RadioGroup
+                          value={this.state.empTravel}
+                          onChange={this.handleChange}
+                        >
+                          <FormControlLabel
+                            value="0"
+                            control={<Radio style={{ color: "#32CD32" }} />}
+                            label="No"
+                          />
+                          <FormControlLabel
+                            value="1"
+                            control={<Radio style={{ color: "#32CD32" }} />}
+                            label="Yes"
+                          />
+                        </RadioGroup>
+                      </FormControl>
                     </Form.Group>
                   </div>
                 </div>
-                <hr /> 
-                  {/*Div used for Certification */}
-                  <div className="u-div">
+                <hr />
+                {/*Div used for Certification */}
+                <div className="u-div">
                   <div className="ulabel">Certification</div>
                   <div className="ucontent-div">
-                    <Form.Group as={Col} controlId="empCert">
-                      <OverlayTrigger placement="left">
-                        <Form.Label>I hereby certify that the responses provided above are true and accurate to the best of my knowledge. </Form.Label>
-                      </OverlayTrigger>
-                      <Form.Control
-                        placeholder="will be checklist button-agreement box"
-                        required={true}
-                        name="checklist"
-                        value={this.state.empCert}
+                    <Form.Group as={Col} controlId="certChecklist">
+                      <Form.Label>
+                        I hereby certify that the responses provided above are
+                        true and accurate to the best of my knowledge.
+                      </Form.Label>
+
+                      <Checklist
+                        data={certData.map((cert) => cert.text)}
+                        checked={this.state.selectedCert}
+                        callback={this.callbackChecklistCert}
                       />
                     </Form.Group>
                     <Button
-                    className="ubutton"
-                  
-                    style={{
-                      marginLeft: "15px",
-                      backgroundColor: "green",
-                      border: "#4E2E84",
-                      height: "50px",
-                      width: "90px",
-                    }}
-                  >
+                      className="ubutton"
+                      style={{
+                        marginLeft: "15px",
+                        backgroundColor: "green",
+                        border: "#4E2E84",
+                        height: "50px",
+                        width: "90px",
+                      }}
+                    >
                       Submit
-            
-                  </Button>
+                    </Button>
                   </div>
-
                 </div>
-               
-
               </Form>
             </div>
           </Tab>
